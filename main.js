f84ea169328fa5918d4d5ffbcdd8baf4dead96c4
Mahim Goyal <mahim97@gmail.com>
2017-04-02 01:28:20 +0530

    Align pagination towards left

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 new Vue({
     el: '#getgist',
     data: {
-      message: 'Hello Vue.js!',
       files: [],
-      showtable: false
+      showtable: false,
     },
     methods: {
       getgistlist: function(page) {
                 this.$http.get('https://api.github.com/gists/public' + '?page=' + (page ? page : '')).then(response => {
                   this.files = response.body;
                   this.showtable = true;
+                  console.log(currentPage);
                   console.log("GET:", 'https://api.github.com/gists/public' + '?page=' + (page ? page : ''));
                 }, response => {
                   // TODO: Give a error response in UI to notify the user
